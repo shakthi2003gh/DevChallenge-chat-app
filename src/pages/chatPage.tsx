db952f65280f27ce1e2bfd661045fc79aca9aa6b
shakthi <shakthikumar123456789@gmail.com>
2023-01-10 14:37:14 +0530

    Create chat section

--- src/pages/chatPage.tsx
+++ src/pages/chatPage.tsx
@@ -1,10 +1,23 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { State } from "../state";
+import { menuClose, menuOpen } from "../state/menu";
 import { Chat, SideBar } from "../sections";
 
 const ChatPage = () => {
+  const selectedGroupName = useSelector(
+    (state: State) => state.entities.selectedGroup.name
+  );
+
+  useEffect(() => {
+    if (selectedGroupName) menuClose();
+    else menuOpen();
+  }, [selectedGroupName]);
+
   return (
     <div className="chat-app">
       <SideBar />
-      <Chat />
+      {selectedGroupName && <Chat />}
     </div>
   );
 };
--- src/sections/chat.tsx
+++ src/sections/chat.tsx
@@ -1,5 +1,42 @@
+import { useEffect } from "react";
+import { State } from "../state";
+import { useSelector } from "react-redux";
+import { menuOpen } from "../state/menu";
+import { Input, Message } from "../components";
+import { MessageInterface } from "../state/messages";
+
 const Chat = () => {
-  return <div className="chat"></div>;
+  const { name: groupName } = useSelector(
+    (state: State) => state.entities.selectedGroup
+  );
+  const messages = useSelector((state: State) => state.messages);
+
+  useEffect(() => {
+    const container = document.querySelector(".messages") as HTMLDivElement;
+    const containerHeight = container.scrollHeight;
+    container.scrollTo({ top: containerHeight, behavior: "smooth" });
+  });
+
+  return (
+    <div className="chat">
+      <div className="header">
+        <span className="material-symbols-rounded menu" onClick={menuOpen}>
+          menu
+        </span>
+        <span>{groupName}</span>
+      </div>
+
+      <div className="messages">
+        {messages.map((message: MessageInterface, i: any) => (
+          <Message key={i} {...message} />
+        ))}
+      </div>
+
+      <div className="messaging-area">
+        <Input buttonName="send" placeholder="Type a message here" />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
